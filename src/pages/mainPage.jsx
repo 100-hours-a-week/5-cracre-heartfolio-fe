21697efefea7dfdc05758d2adb9772bb1cd6f48d
minji <mjkim.peach@gmail.com>
2024-08-16 17:25:07 +0900

    add popularChart in mainpage

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import Header from "../components/header";
 import ButtomNavigation from "../components/bottomNavigation";
+import PopularStockBox from "../components/popularStockBox";
+import PopularChart from "../components/popularChart";
 
 function MainPage() {
   const navigate = useNavigate();
@@ -9,18 +11,30 @@ function MainPage() {
       <Header />
       <div className="mx-auto max-w-[390px] px-3 mt-[34px]">
         <div className="flex justify-around">
-          <button className="bg-btnNoClickColor rounded-lg w-[110px] h-[50px] text-sm" onClick={() => navigate('/portfolio')}>
+          <button
+            className="bg-btnNoClickColor rounded-lg w-[110px] h-[50px] text-sm"
+            onClick={() => navigate("/portfolio")}
+          >
             내 포트폴리오
           </button>
-          <button className="bg-btnNoClickColor rounded-lg w-[110px] h-[50px] text-sm" onClick={() => navigate('/ranking')}>
+          <button
+            className="bg-btnNoClickColor rounded-lg w-[110px] h-[50px] text-sm"
+            onClick={() => navigate("/ranking")}
+          >
             랭킹
           </button>
-          <button className="bg-btnNoClickColor rounded-lg w-[110px] h-[50px] text-sm" onClick={() => navigate('/intereststock')}>
+          <button
+            className="bg-btnNoClickColor rounded-lg w-[110px] h-[50px] text-sm"
+            onClick={() => navigate("/intereststock")}
+          >
             모의투자
           </button>
         </div>
+        <div>
+            <PopularChart/> 
+        </div>
       </div>
-      <ButtomNavigation/>
+      <ButtomNavigation />
     </>
   );
 }
