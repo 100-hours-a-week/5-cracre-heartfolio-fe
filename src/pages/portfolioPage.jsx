68b19026e881e317e20f562c6e2dc6803d9083d4
minji <mjkim.peach@gmail.com>
2024-08-19 18:03:30 +0900

    add mock data of transactionHistory

--- src/pages/portfolioPage.jsx
+++ src/pages/portfolioPage.jsx
@@ -11,8 +11,10 @@ function Portfolio() {
   const [activeTab, setActiveTab] = useState(1);
   console.log("Active Tab:", activeTab);
 
-  // const {data, error, loading} = useFetch("http://localhost:8080/api/portfolio/"+userId);
-  // const {data, error, loading} = useFetch("http://localhost:8080/api/portfolio/"+userId+"/stock");
+  // const {cash_data, error, loading} = useFetch("http://localhost:8080/api/portfolio/"+userId);
+  // const {asset_data, error, loading} = useFetch("http://localhost:8080/api/portfolio/"+userId+"/stock");
+  // const {transaction_data, error, loading} = useFetch("http://localhost:8080/api/portfolio/investInfo");
+
   const cash_data = {
     cash: 75121616,
     total_purchase: 35000616,
@@ -76,6 +78,64 @@ function Portfolio() {
       ],
     },
   };
+  const transaction_data = {
+    data: [
+      {
+        stock_id: 1,
+        name: "NVIDIA",
+        order_category: "buy",
+        order_date: "2024-08-14 13:16:00",
+        order_amount: 100,
+        order_price: 30330,
+        total_amount: 303300,
+      },
+      {
+        stock_id: 2,
+        name: "Apple",
+        order_category: "sell",
+        order_date: "2024-08-15 09:30:00",
+        order_amount: 50,
+        order_price: 15000,
+        total_amount: 750000,
+      },
+      {
+        stock_id: 3,
+        name: "Tesla",
+        order_category: "buy",
+        order_date: "2024-08-16 10:45:00",
+        order_amount: 75,
+        order_price: 68000,
+        total_amount: 5100000,
+      },
+      {
+        stock_id: 4,
+        name: "AMD",
+        order_category: "buy",
+        order_date: "2024-08-17 11:22:00",
+        order_amount: 200,
+        order_price: 12000,
+        total_amount: 2400000,
+      },
+      {
+        stock_id: 5,
+        name: "Microsoft",
+        order_category: "sell",
+        order_date: "2024-08-18 14:50:00",
+        order_amount: 30,
+        order_price: 32000,
+        total_amount: 96023442000,
+      },
+      {
+        stock_id: 6,
+        name: "Microsoft",
+        order_category: "sell",
+        order_date: "2024-08-18 14:50:00",
+        order_amount: 30,
+        order_price: 32000,
+        total_amount: 960000,
+      },
+    ],
+  };
 
   return (
     <>
@@ -109,8 +169,8 @@ function Portfolio() {
       </div>
       <div className="mx-auto max-w-[370px] bg-backColor p-4">
         <div role="tabpanel" className="tab-content block">
-          {activeTab === 1 && <AssetConfiguration data={asset_data}/>}
-          {activeTab === 2 && <TransactionHistory />}
+          {activeTab === 1 && <AssetConfiguration data={asset_data} />}
+          {activeTab === 2 && <TransactionHistory data={transaction_data} />}
           {activeTab === 3 && <Holdings />}
         </div>
       </div>
