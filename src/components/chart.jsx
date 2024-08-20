3bef48ebf8bb00076a8866afbd12f77e7e266b14
minji <mjkim.peach@gmail.com>
2024-08-20 16:57:16 +0900

    add buy modal

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 const socket = io("http://localhost:3000");
 
 const { default: TradingViewWidget } = require("./tradingViewWidget");
 
 function Chart(props) {
+  const { id } = useParams();
+  // const {money_data, error, loading} = useFetch("http://localhost:8080/api/portfolio/"+userId);
+
   const [quantity, setQuantity] = useState("");
   const [currentPrice, setCurrentPrice] = useState(0); // 주식 현재가를 저장할 상태
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 관리
+  const [orderDetails, setOrderDetails] = useState({
+    quantity: 0,
+    price: 0,
+    total: 0,
+  });
+
+  const money_data = {
+    cash: 75121616,
+    total_purchase: 35000616,
+    total_amount: 200000000,
+    total_value: 151152125,
+    profitRate: -10.4,
+  };
 
   useEffect(() => {
     // 웹소켓을 통해 주식 현재가를 수신하는 로직
@@ -26,13 +44,52 @@ function Chart(props) {
       setQuantity(e.target.value);
     }
   };
+  const total_money = currentPrice * quantity;
+
   const isDisabled = quantity <= 0;
   const buttonStyle = isDisabled ? "bg-[#FEF0F2]" : "bg-[#FFE7E9]";
+  function buy() {
+    if (quantity * currentPrice > money_data.cash) {
+      alert("본인 캐시를 확인해주세요");
+      return;
+    } else {
+      // fetch("http://localhost:8080/api/invest/order", {
+      //   credentials: "include",
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     id: id,
+      //     quantity: quantity,
+      //     price: currentPrice,
+      //   }),
+      // }).then((res) => {
+      //   if (res.ok) {
+      //     setOrderDetails({
+      //       quantity: quantity,
+      //       price: currentPrice,
+      //       total: quantity * currentPrice,
+      //     });
+      //     setIsModalOpen(true);
+      //   }
+      // });
+    }
+    setOrderDetails({
+      quantity: quantity,
+      price: currentPrice,
+      total: quantity * currentPrice,
+    });
+    setIsModalOpen(true);
+  }
+  function closeModal() {
+    setIsModalOpen(false);
+  }
   return (
     <>
       <div className="mx-auto max-w-[370px]">
         <div></div>
-          <p className="pb-2">{currentPrice} KRW</p>
+        <p className="pb-2">{currentPrice} KRW</p>
         <TradingViewWidget symbol={props.info.symbol} />
         <div>
           <div className="flex w-[370px]">
@@ -65,21 +122,22 @@ function Chart(props) {
             <div className="flex items-center w-1/2">
               <p>총액</p>
               <p className="h-[30px] w-[130px] content-center text-right text-xs">
-                KRW
+                {total_money} KRW
               </p>
             </div>
             <div className="flex items-center w-1/2">
               <p>내 캐시</p>
               <p className="h-[30px] w-[130px] content-center text-right text-xs">
-                KRW
+                {money_data.cash} KRW
               </p>
             </div>
           </div>
         </div>
-        <div className="w-[390px] text-center mt-9">
+        <div className="w-[350px] text-center mt-9">
           <button
             className={`${buttonStyle} h-10 w-[140px] mx-3 rounded-md text-sm`}
             disabled={isDisabled}
+            onClick={() => buy()}
           >
             매수
           </button>
@@ -91,6 +149,27 @@ function Chart(props) {
           </button>
         </div>
       </div>
+
+      {/* 매수 완료 모달 */}
+      {isModalOpen && (
+        <div
+          id="my_modal_5"
+          className="modal modal-bottom sm:modal-middle "
+          open
+        >
+          <div className="modal-box text-center" style={{ width: '350px' }}>
+            <h3 className="font-bold text-lg p-3">매수 거래 체결 완료!</h3>
+            <p>수량 : {orderDetails.quantity}</p>
+            <p>가격 : {orderDetails.price} KRW</p>
+            <p>총액 : {orderDetails.total} KRW</p>
+            <div className="modal-action">
+              <button className="btn" onClick={closeModal}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
