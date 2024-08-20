a4f684fa551e96cbb3a481341ad4361c43af3a2d
TakYeji <ivorycream01@gmail.com>
2024-08-20 14:14:47 +0900

    메인페이지 수정,stocktype수정

--- src/components/stocktype.js
+++ src/components/stocktype.js
@@ -1,6 +1,7 @@
 const tabs = [
-  { name: "관심종목", href: "/intereststock", current: false },
-  { name: "인기종목", href: "/popularstock", current: false },
+  { name: "관심종목", href: "/intereststock", current: false, isIcon: false },
+  { name: "인기종목", href: "/popularstock", current: false, isIcon: false },
+  { name: "search", href: "/search", isIcon: true, current: false } // 아이콘 항목 추가
 ];
 
 function classNames(...classes) {
@@ -9,11 +10,11 @@ function classNames(...classes) {
 
 export default function Stocktype() {
   return (
-    <div className="mx-auto max-w-[390px] px-3  z-50 flex">
-      <div className="hidden sm:block">
+    <div className="mx-auto max-w-[390px] px-3 z-50 flex">
+      <div className="hidden sm:block w-full">
         <nav
           aria-label="Tabs"
-          className="isolate flex divide-x divide-gray-200 rounded-lg shadow"
+          className="isolate flex divide-x divide-gray-200 shadow"
         >
           {tabs.map((tab, tabIdx) => (
             <a
@@ -26,10 +27,17 @@ export default function Stocktype() {
                   : "text-gray-500 hover:text-gray-700",
                 tabIdx === 0 ? "rounded-l-lg" : "",
                 tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
-                "group relative min-w-0 flex-1  bg-white px-4 py-4 text-center  hover:bg-gray-50 focus:z-10 w-40"
+                "group relative min-w-0 flex items-center justify-center bg-white px-4 py-4 text-center hover:bg-gray-50 focus:z-10",
+                tab.isIcon ? "flex-none w-16" : "flex-grow" // 아이콘의 경우 고정 너비, 나머지는 flex-grow로 동일하게 분배
               )}
             >
-              <span>{tab.name}</span>
+              {tab.isIcon ? (
+                <span className="material-symbols-outlined text-2xl">
+                  search
+                </span>
+              ) : (
+                <span>{tab.name}</span>
+              )}
               <span
                 aria-hidden="true"
                 className={classNames(
@@ -41,12 +49,6 @@ export default function Stocktype() {
           ))}
         </nav>
       </div>
-      <a
-        href="/search" //검색 화면으로 이동
-        className="material-symbols-outlined px-4 py-4 text-gray-500 hover:text-gray-700 "
-      >
-        search
-      </a>
     </div>
   );
 }
