e37ebf3fbf7c106c9f2cc30ca0550535307faa25
minji <mjkim.peach@gmail.com>
2024-08-21 14:28:01 +0900

    add mock data of stockHistory

--- src/components/box/stockHistoryBox.js
+++ src/components/box/stockHistoryBox.js
@@ -1,18 +1,18 @@
-function StockHistoryBox() {
+function StockHistoryBox(props) {
   return (
     <>
         <div className="w-[350px] mx-auto justify-between">
           <div className="flex w-fit">
             <div>매도 일시 :</div>
-            <div className="ml-1">YYYY-MM-DD HH-MM-SS</div>
+            <div className="ml-1">{props.orderDate}</div>
           </div>
           <div className="flex w-fit">
             <div>거래 가격 :</div>
-            <div className="ml-1">23,000</div>
+            <div className="ml-1">{props.orderPrice.toLocaleString()}</div>
           </div>
           <div className="flex w-fit">
             <div>수량 :</div>
-            <div className="ml-1">3</div>
+            <div className="ml-1">{props.orderAmount}</div>
           </div>
         </div>
     </>
