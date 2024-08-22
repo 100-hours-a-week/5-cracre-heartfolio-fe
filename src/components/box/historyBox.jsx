828292bc7637db4e694258c1c436cfcba589d18f
minji <mjkim.peach@gmail.com>
2024-08-22 16:39:11 +0900

    edit history date

--- src/components/box/historyBox.jsx
+++ src/components/box/historyBox.jsx
@@ -6,7 +6,7 @@ function HistoryBox(props) {
       return "매도";
     }
   }
-  function money_change(money) {
+  const moneyChange = (money) => {
     if (money >= 1000000) {
       // 1,000,000 이상
       let mValue = money / 1000000; // M 단위로 변환
@@ -15,44 +15,49 @@ function HistoryBox(props) {
       return money.toLocaleString(); // 기본적으로 1,000 단위로 콤마를 추가
     }
   }
+
+  const formatDate = (date) => {
+    return date.replace("T", " ");
+  };
+
   return (
     <>
       <div className="w-[330px] h-[80px] flex justify-center items-center">
         <div className="flex w-[320px] justify-between items-center">
           <div className="w-[180px]">
-            <div className="text-sm">{props.order_date}</div>
+            <div className="text-sm">{formatDate(props.orderDate)}</div>
             <div className="flex w-[140px] justify-between">
               <div>{props.name}</div>
               <div
                 className={`${
-                  props.order_category == "buy"
+                  props.orderCategory == "buy"
                     ? "text-redColor" // 매수일 때, 빨강색
                     : "text-blueColor" // 매도일 때, 파랑색
                 }`}
               >
-                {sellOrBuy(props.order_category)}
+                {sellOrBuy(props.orderCategory)}
               </div>
             </div>
           </div>
           <div className="w-[140px]">
             <div className="flex justify-between">
               <div className="text-sm">평단가</div>
-              <div className="text-sm">{money_change(props.order_price)}</div>
+              <div className="text-sm">{moneyChange(props.orderPrice)}</div>
             </div>
             <div className="flex justify-between">
               <div className="text-sm">체결수량</div>
-              <div className="text-sm">{props.order_amount}</div>
+              <div className="text-sm">{props.orderAmount}</div>
             </div>
             <div className="flex justify-between">
               <div className="text-sm">체결금액</div>
               <div
                 className={`text-sm ${
-                  props.order_category == "buy"
+                  props.orderCategory == "buy"
                     ? "text-redColor" // 매수일 때, 빨강색
                     : "text-blueColor" // 매도일 때, 파랑색
                 }`}
               >
-                {money_change(props.total_amount)}
+                {moneyChange(props.totalAmount)}
               </div>
             </div>
           </div>
