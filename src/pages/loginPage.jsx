2305d3c493ce2b1be91e3ff17d3b857ce97c042e
minji <mjkim.peach@gmail.com>
2024-08-24 13:20:19 +0900

    login 성공!

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from "react";
-import ButtomNavigation from "../components/bottomNavigation";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY; //REST API KEY
-  const REDIRECT_URI = `https://heartfolio.site/oauth`;  // oauth 요청 URL
+  const navigate = useNavigate();
 
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.REACT_APP_REST_API_KEY);
+    }
+  }, []);
+
   const handleLogin = () => {
-    window.location.href = kakaoURL;
+    window.Kakao.Auth.authorize({
+      redirectUri: `${window.location.origin}/oauth`,
+    });
   };
-  https: return (
+
+  return (
     <>
       <div>
         <div className="flex flex-col justify-center items-center mx-auto max-w-[390px] px-3 my-[100px]">
@@ -17,10 +24,12 @@ const LoginPage = () => {
             src="/assets/images/heartfolioLogo.webp"
             alt="heartfolio logo"
             className="w-[180px] my-10 rounded-lg"
-          ></img>
-          
+          />
+
           <p className="text-3xl m-5 font-semibold">HeartFoilo</p>
-          <p className="m-2 font-medium ">기부로 마음을 공유할 수 있는 모의투자</p>
+          <p className="m-2 font-medium ">
+            기부로 마음을 공유할 수 있는 모의투자
+          </p>
         </div>
       </div>
       <div className="flex flex-col items-center">
@@ -34,7 +43,9 @@ const LoginPage = () => {
         <p id="token-result" className="mt-4 text-sm text-gray-700"></p>
       </div>
       <a href="/">
-      <p className="text-xs m-5 text-center text-slate-400	"> 비회원으로 이용하기</p>
+        <p className="text-xs m-5 text-center text-slate-400	">
+          비회원으로 이용하기
+        </p>
       </a>
     </>
   );
