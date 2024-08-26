c2885a43dd55821025c2a003fe15d7933c6f656d
minji <mjkim.peach@gmail.com>
2024-08-26 13:49:18 +0900

    fix: portfolio page error (No useFetch)

--- src/components/holdings.js
+++ src/components/holdings.js
@@ -1,17 +1,43 @@
+import { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import HoldingsBox from "./box/holdingsBox";
 
 function Holdings() {
   const token = localStorage.getItem("access_token");
-  const { data, error, loading } = useFetch(
-    "https://heartfolio.site/api/portfolio/totalStocks",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`, // 토큰을 헤더에 추가
-        "Content-Type": "application/json", // 선택 사항, API 요구 사항에 따라 설정
-      },
-    }
-  );
+  // 데이터 가져오기 상태 관리
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true); // 데이터 가져오기 시작 전에 로딩 상태 설정
+      try {
+        const response = await fetch(
+          "https://heartfolio.site/api/portfolio/totalStocks",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`, // 토큰을 헤더에 추가
+              "Content-Type": "application/json", // 선택 사항, API 요구 사항에 따라 설정
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(response.statusText); // 응답이 정상적이지 않으면 에러 발생
+        }
+
+        const result = await response.json();
+        setData(result); // 가져온 데이터를 상태에 설정
+      } catch (err) {
+        setError(err); // 에러 발생 시 상태에 설정
+      } finally {
+        setLoading(false); // 데이터 가져오기 완료 후 로딩 상태 해제
+      }
+    };
+
+    fetchData();
+  }, [token]);
   return (
     <>
       <div className="mx-auto max-w-[350px] py-4 pb-8">
