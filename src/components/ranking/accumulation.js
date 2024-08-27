151242805dc7a0c5eaa84a7a346de8d3a1e65ef3
minji <mjkim.peach@gmail.com>
2024-08-27 22:01:15 +0900

    ranking page with 주석

---
+++ src/components/ranking/accumulation.js
@@ -0,0 +1,60 @@
+// function Accumulation() {
+//   return (
+//     <>
+//       <div>
+//         <div className="w-[350px]">
+//           <table className="table">
+//             <tbody>
+//               {/* row 1 */}
+//               <tr>
+//                 <td>
+//                   <div className="text-base">1</div>
+//                 </td>
+//                 <td>
+//                   <div className="flex items-center gap-3">
+//                     <div className="avatar">
+//                       <div className="mask mask-squircle h-12 w-12">
+//                         <img
+//                           src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+//                           alt="profile image"
+//                         />
+//                       </div>
+//                     </div>
+//                     <div>
+//                       <div className="font-bold">닉네임</div>
+//                     </div>
+//                   </div>
+//                 </td>
+//                 <td className="text-right">수익률%</td>
+//               </tr>
+//               {/* row 2 */}
+//               <tr>
+//                 <td>
+//                   <div className="text-base">2</div>
+//                 </td>
+//                 <td>
+//                   <div className="flex items-center gap-3">
+//                     <div className="avatar">
+//                       <div className="mask mask-squircle h-12 w-12">
+//                         <img
+//                           src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+//                           alt="profile image"
+//                         />
+//                       </div>
+//                     </div>
+//                     <div>
+//                       <div className="font-bold">닉네임</div>
+//                     </div>
+//                   </div>
+//                 </td>
+//                 <td className="text-right">수익률%</td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Accumulation;
