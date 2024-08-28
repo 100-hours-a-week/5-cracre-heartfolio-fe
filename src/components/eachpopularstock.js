89bf320c6b570f6bfae19ec56a6e8ea0c66c34ac
minji <mjkim.peach@gmail.com>
2024-08-28 13:57:08 +0900

    edit padding

--- src/components/eachpopularstock.js
+++ src/components/eachpopularstock.js
@@ -13,7 +13,7 @@ export default function Eachpopularstock(props) {
   return (
     //onClick={handleClick} 아래줄에 넣어주세요!!
     <div
-      className="flex flex-row mx-auto max-w-[370px] m-3 bg-white hover:bg-gray-50 "
+      className="flex flex-row mx-auto max-w-[350px] m-3 bg-white hover:bg-gray-50 "
       onClick={() => handleClick(props.stockId)}
     >
       {/* 순위 */}
