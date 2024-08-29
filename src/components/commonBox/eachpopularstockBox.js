1a3b66601151ac3baa0df4f8d3b1490a435dcd2a
TakYeji <ivorycream01@gmail.com>
2024-08-29 09:31:21 +0900

    글 사이즈 변경중

--- src/components/commonBox/eachpopularstockBox.js
+++ src/components/commonBox/eachpopularstockBox.js
@@ -17,11 +17,11 @@ export default function EachpopularStockBox(props) {
       onClick={() => handleClick(props.stockId)}
     >
       {/* 순위 */}
-      <p className="text-gray-600 w-10 py-3 text-center content-center font-bold text-xl">{props.rank}</p>
+      <p className="text-gray-600 w-10 py-3 text-center content-center font-bold text-lg">{props.rank}</p>
       {/* 내용 */}
       <div className="text-gray-600 ml-4 w-[300px]">
         {/* 종목이름 */}
-        <h1 className="text-gray-600 font-medium text-xl text-[15px]">{props.stockKorea} ({props.stockName})</h1>
+        <h1 className="text-gray-600 font-medium text-lg text-[15px]">{props.stockKorea} ({props.stockName})</h1>
         {/* 종목정보 */}
         <div className="flex flex-row mt-1">
           {/* 현재가 */}
