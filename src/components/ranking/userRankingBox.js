daf54e6d122f7820585e11353f5eafd21647bec9
minji <mjkim.peach@gmail.com>
2024-09-09 16:56:18 +0900

    change ranking box to one component

---
+++ src/components/ranking/userRankingBox.js
@@ -0,0 +1,37 @@
+function UserRankingBox(props) {
+    return (
+      <>
+        <div>
+          <div className="w-[350px]">
+            <table className="table">
+              <tbody>
+                {props.data?.map((item, index) => (
+                  <tr key={index} className="border-b-1 border-gray-300">
+                    <td>
+                      <div className="text-base text-bold text-gray-800">{index + 1}</div>
+                    </td>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img src={""} alt="profile image" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold text-gray-600">{item.name}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className={`${parseFloat(item.percentage)>0 ? 'text-redColor' : parseFloat(item.percentage)<0 ?'text-blueColor' : 'text-gray-600'} text-right`}>{item.percentage}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default UserRankingBox;
+  
