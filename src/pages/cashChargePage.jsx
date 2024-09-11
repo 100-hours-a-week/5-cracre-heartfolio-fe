9d16296a85102543c16654bd2fd886a83d3673a2
minji <mjkim.peach@gmail.com>
2024-09-11 21:40:43 +0900

    can use cashChargePage after login

--- src/pages/cashChargePage.jsx
+++ src/pages/cashChargePage.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react"; // Import useState
+import React, { useEffect, useState } from "react"; // Import useState
 import Lottie from "lottie-react";
 import ButtomNavigation from "../components/common/bottomNavigation";
 import Header from "../components/common/header";
 import ContructionAnimation from "../assets/animations/construction.json";
 import CategoryModal from "../components/cashCharge/categoryModal";
 import CheckModal from "../components/cashCharge/checkModal";
+import alertAnimation from "../assets/animations/alert.json";
 
 function CashChargePage() {
   const [selectedAmount, setSelectedAmount] = useState(null);
   const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false);
   const [isCheckModalOpen, setIsCheckModalOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const showModal = (amount) => {
     setSelectedAmount(amount);
@@ -31,23 +40,39 @@ function CashChargePage() {
     <>
       <Header />
       <div className="pt-[90px] min-h-screen bg-white text-center flex flex-col items-center">
-        <div className="text-xl text-gray-600 font-TmoneyRoundWindExtraBold">
-          캐시충전 서비스
-        </div>
-        <button
-          className="btn w-[300px] h-[90px] mt-11 bg-btnNoClickColor border-0 text-gray-600 justify-between hover:bg-btnClickColor"
-          onClick={() => showModal(1000)}
-        >
-          <div className="text-">1000원</div>
-          <div>1,200,000원 캐시</div>
-        </button>
-        <button
-          className="btn w-[300px] h-[90px] mt-11 bg-btnNoClickColor border-0 text-gray-600 justify-between hover:bg-btnClickColor"
-          onClick={() => showModal(2000)}
-        >
-          <div className="text-">2000원</div>
-          <div>2,400,000원 캐시</div>
-        </button>
+        {isAuthenticated == true ? (
+          <>
+            <div className="text-xl text-gray-600 font-TmoneyRoundWindExtraBold">
+              캐시충전 서비스
+            </div>
+            <button
+              className="btn w-[300px] h-[90px] mt-11 bg-btnNoClickColor border-0 text-gray-600 justify-between hover:bg-btnClickColor"
+              onClick={() => showModal(1000)}
+            >
+              <div className="text-">1000원</div>
+              <div>1,200,000원 캐시</div>
+            </button>
+            <button
+              className="btn w-[300px] h-[90px] mt-11 bg-btnNoClickColor border-0 text-gray-600 justify-between hover:bg-btnClickColor"
+              onClick={() => showModal(2000)}
+            >
+              <div className="text-">2000원</div>
+              <div>2,400,000원 캐시</div>
+            </button>
+          </>
+        ) : (
+          <div className="flex flex-col justify-center items-center h-full">
+            <div className="w-80 h-80">
+              <Lottie animationData={alertAnimation} loop={true} />
+            </div>
+            <div className="font-bold text-lg text-gray-400">
+              로그인 후 이용 가능한 서비스입니다
+            </div>
+            <a href="/login" className="pt-5 text-gray-400 hover:text-gray-600">
+              로그인 하러가기
+            </a>
+          </div>
+        )}
       </div>
       {/* 기부 영역 선택 모달창 */}
       {isCategoryModalOpen && (
