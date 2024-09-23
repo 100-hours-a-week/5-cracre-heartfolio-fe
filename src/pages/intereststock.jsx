1b59a6bfac43aad03bf258944b9b91532f98ade2
minji <mjkim.peach@gmail.com>
2024-09-23 14:54:58 +0900

    intereststock add using useFetch

--- src/pages/intereststock.jsx
+++ src/pages/intereststock.jsx
@@ -12,7 +12,6 @@ function Intereststock() {
   const token = localStorage.getItem("access_token");
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   useEffect(() => {
     if (token) {
       setIsAuthenticated(true);
@@ -20,71 +19,72 @@ function Intereststock() {
   }, []);
 
   // 데이터 가져오기 상태 관리
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const {data, error, loading} = useFetch(`${process.env.REACT_APP_API_URI}/stock/favorites`);
+  // const [data, setData] = useState(null);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true); // 데이터 가져오기 시작 전에 로딩 상태 설정
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_API_URI}/stock/favorites`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`, // 토큰을 헤더에 추가
-              "Content-Type": "application/json", // 선택 사항, API 요구 사항에 따라 설정
-            },
-          }
-        );
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     setLoading(true); // 데이터 가져오기 시작 전에 로딩 상태 설정
+  //     try {
+  //       const response = await fetch(
+  //         `${process.env.REACT_APP_API_URI}/stock/favorites`,
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${token}`, // 토큰을 헤더에 추가
+  //             "Content-Type": "application/json", // 선택 사항, API 요구 사항에 따라 설정
+  //           },
+  //         }
+  //       );
 
-        if (response.status === 401) {
-          // Access token 만료 -> refresh token으로 새 access token 요청
-          const refreshToken = localStorage.getItem("refresh_token");
-          const refreshResponse = await fetch(
-            `${process.env.REACT_APP_API_URI}/auth/refresh-token`,
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ refreshToken: refreshToken }),
-            }
-          );
+  //       if (response.status === 401) {
+  //         // Access token 만료 -> refresh token으로 새 access token 요청
+  //         const refreshToken = localStorage.getItem("refresh_token");
+  //         const refreshResponse = await fetch(
+  //           `${process.env.REACT_APP_API_URI}/auth/refresh-token`,
+  //           {
+  //             method: "POST",
+  //             headers: { "Content-Type": "application/json" },
+  //             body: JSON.stringify({ refreshToken: refreshToken }),
+  //           }
+  //         );
 
-          if (refreshResponse.status === 200) {
-            const data = await refreshResponse.json();
-            localStorage.setItem("access_token", data.accessToken); // 새 access token 저장
+  //         if (refreshResponse.status === 200) {
+  //           const data = await refreshResponse.json();
+  //           localStorage.setItem("access_token", data.accessToken); // 새 access token 저장
 
-            // 새로운 access token으로 원래 요청 다시 시도
-            response = await fetch(
-              `${process.env.REACT_APP_API_URI}/stock/favorites`,
-              {
-                headers: {
-                  Authorization: `Bearer ${localStorage.getItem(
-                    "access_token"
-                  )}`, // 새 access token 사용
-                  "Content-Type": "application/json",
-                },
-              }
-            );
-          } else {
-            // refresh token도 만료되거나 오류가 있으면 로그인 페이지로 이동
-            localStorage.removeItem("access_token");
-            window.location.href = "/login";
-            return;
-          }
-        }
+  //           // 새로운 access token으로 원래 요청 다시 시도
+  //           response = await fetch(
+  //             `${process.env.REACT_APP_API_URI}/stock/favorites`,
+  //             {
+  //               headers: {
+  //                 Authorization: `Bearer ${localStorage.getItem(
+  //                   "access_token"
+  //                 )}`, // 새 access token 사용
+  //                 "Content-Type": "application/json",
+  //               },
+  //             }
+  //           );
+  //         } else {
+  //           // refresh token도 만료되거나 오류가 있으면 로그인 페이지로 이동
+  //           localStorage.removeItem("access_token");
+  //           window.location.href = "/login";
+  //           return;
+  //         }
+  //       }
 
-        const result = await response.json();
-        setData(result); // 가져온 데이터를 상태에 설정
-      } catch (err) {
-        setError(err); // 에러 발생 시 상태에 설정
-      } finally {
-        setLoading(false); // 데이터 가져오기 완료 후 로딩 상태 해제
-      }
-    };
+  //       const result = await response.json();
+  //       setData(result); // 가져온 데이터를 상태에 설정
+  //     } catch (err) {
+  //       setError(err); // 에러 발생 시 상태에 설정
+  //     } finally {
+  //       setLoading(false); // 데이터 가져오기 완료 후 로딩 상태 해제
+  //     }
+  //   };
 
-    fetchData();
-  }, [token]);
+  //   fetchData();
+  // }, [token]);
 
   return (
     <>
