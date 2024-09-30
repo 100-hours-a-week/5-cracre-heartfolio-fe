b31be76a08e7ccf2ecbe79df2db984fe99740571
minji <mjkim.peach@gmail.com>
2024-09-30 22:53:14 +0900

    add kakaoRedirect loading animation

--- src/pages/kakaoRedirect.jsx
+++ src/pages/kakaoRedirect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Loading } from "../components/common/loading";
 
 const KakaoRedirect = () => {
   const navigate = useNavigate();
@@ -19,7 +20,7 @@ const KakaoRedirect = () => {
         .then((data) => {
           if (data && data.token && data.token.access_token) {
             localStorage.setItem("access_token", data.token.access_token);
-            localStorage.setItem('refresh_token', data.token.refresh_token);
+            localStorage.setItem("refresh_token", data.token.refresh_token);
             navigate("/"); // 로그인 후 메인 페이지로 리다이렉트
           } else {
             console.error("토큰이 없습니다.", data);
@@ -35,7 +36,7 @@ const KakaoRedirect = () => {
 
   return (
     <div className="pt-3 text-center min-h-screen bg-white">
-      <p>로그인 중...</p>
+      <Loading />
     </div>
   );
 };
