cd229abb4aa569f66696b772a92843847c6805e3
minji <mjkim.peach@gmail.com>
2024-10-03 14:37:54 +0900

    edit get luckyData

--- src/components/main/luckyBox.js
+++ src/components/main/luckyBox.js
@@ -5,7 +5,6 @@ import { useState } from "react";
 function LuckyBox() {
   const [showLuck, setShowLuck] = useState(false);
   const [luckData, setLuckData] = useState(null);
-  const [fortuneMessage, setFortuneMessage] = useState(null);
   const token = localStorage.getItem("access_token");
 
   const isUnauthorized = !token;
@@ -14,7 +13,7 @@ function LuckyBox() {
     setShowLuck(true);
     if (!isUnauthorized) {
       try {
-        const res = await fetchWithToken(
+        let res = await fetchWithToken(
           `${process.env.REACT_APP_API_URI}/fortune`,
           {
             method: "POST",
@@ -24,8 +23,8 @@ function LuckyBox() {
             },
           }
         );
-        fortuneMessage = res?.message;
-        setLuckData(fortuneMessage); // 객체의 message 값만 설정
+        const result = await res.json();
+
         if (res.status === 401) {
           const refreshToken = localStorage.getItem("refresh_token");
           const refreshResponse = await fetch(
@@ -51,8 +50,8 @@ function LuckyBox() {
             });
 
             if (res.ok) {
-              fortuneMessage = res?.message;
-              setLuckData(fortuneMessage);
+              const newResult = await res.json();
+              setLuckData(newResult?.message);
             }
           } else {
             // refresh token도 만료되거나 오류가 있으면 로그인 페이지로 이동
@@ -60,6 +59,8 @@ function LuckyBox() {
             localStorage.removeItem("refresh_token");
             return;
           }
+        } else {
+          setLuckData(result?.message);
         }
       } catch (error) {
         setLuckData("운세 데이터를 가져올 수 없습니다. 다시 시도해주세요");
