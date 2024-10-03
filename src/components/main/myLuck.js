a1d85cc289804b84c157dadd1bcedd69c01988d3
minji <mjkim.peach@gmail.com>
2024-10-03 15:01:43 +0900

    change luck font

--- src/components/main/myLuck.js
+++ src/components/main/myLuck.js
@@ -1,6 +1,6 @@
 function MyLuck(props) {
   return (
-    <div className="flex items-center justify-center pt-[20px] h-20 p-6 mt-2 rounded-xl bg-gradient-to-r from-amber-200/50 to-pink-200/50">
+    <div className="flex items-center justify-center font-RixXladywatermelonR pt-[20px] h-20 p-6 mt-2 rounded-xl bg-gradient-to-r from-amber-200/50 to-pink-200/50">
       {props.data}
     </div>
   );
